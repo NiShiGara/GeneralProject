d92adaca8268699ee01b15027217bcca0e0485ec
NiShiGara <sorokina.pd@students.dvfu.ru>
2021-11-24 23:06:19 +1000

    Убейте меня

--- npm/npm.js
+++ npm/npm.js
@@ -7,6 +7,9 @@ var sentence = casual.sentence;
 
 // Generate random city name
 var city = casual.city;
-
+var text = casual.text                   // 'Nemo tempore natus non accusamus eos placeat nesciunt. et fugit ut odio nisi dolore non ... (long text)'
+var desc = casual.description
 console.log(city);
 console.log(sentence);
+console.log(text);
+console.log(desc);
